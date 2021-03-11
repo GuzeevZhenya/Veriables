7d1ea8651336c18aad2e51d6f20fd6e6dad0d59f
GuzeevZhenya <shyst.evgeny@mail.ru>
2021-03-11 09:06:17 +0300

    changed the solution of the problem using css vars

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const inputs = document.querySelectorAll('.controls input');
 const changeButton = document.querySelector('.change-button');
 const body = document.querySelector('body');
+const label = document.querySelectorAll('label');
+
+let color = getComputedStyle(document.body);
+
+let darkTheme = color.getPropertyValue('--darkInput');
+let lightTheme = color.getPropertyValue('--whiteBackground')
+
 
 
 inputs.forEach(input => input.addEventListener('change', handleUpdate));
@@ -15,13 +22,22 @@ function handleUpdate() {
 
 
 function darkOrLight() {
-    body.classList.toggle('night');
-    if (body.classList.contains('night')) {
-        changeButton.style.background = "url('sun.png') no-repeat 15px 15px";
-    } else {
-        changeButton.style.background = "url('moon.png') no-repeat 50px 12px";
+    body.style.setProperty('background-color', darkTheme);
+    for (let i = 0; i < label.length; i++) {
+        label[i].style.setProperty('color', lightTheme)
     }
-
 }
+
+
+
+// function darkOrLight() {
+//     body.classList.toggle('night');
+//     if (body.classList.contains('night')) {
+//         changeButton.style.background = "url('sun.png') no-repeat 15px 15px";
+//     } else {
+//         changeButton.style.background = "url('moon.png') no-repeat 50px 12px";
+//     }
+// }
+
 
 changeButton.addEventListener('click', darkOrLight)
